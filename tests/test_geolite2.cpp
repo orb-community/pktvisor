95c0bab04c588352b36452b62bbcee3c2f29c0ca
Shannon Weyrick <sweyrick@nsone.net>
2020-02-23 15:35:40 -0500

    add geoip test

---
+++ tests/test_geolite2.cpp
@@ -0,0 +1,19 @@
+#include <catch2/catch.hpp>
+#include "GeoLite2PP.h"
+
+TEST_CASE("GeoLite2PP", "[geo]")
+{
+
+    SECTION("basic Geo lookup")
+    {
+        GeoLite2PP::DB db("fixtures/ipdb.mmdb");
+        uint16_t country, prov, isp, city;
+        db.get_geoinfo("36.110.59.146", country, prov, isp, city);
+        CHECK(country == 47);
+        CHECK(isp == 1);
+        CHECK(prov == 1);
+    }
+
+
+}
+
