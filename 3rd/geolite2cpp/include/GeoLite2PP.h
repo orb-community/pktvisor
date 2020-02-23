cc9b5c1213b3cccb6085c8530f8634dab2a9b5df
Shannon Weyrick <sweyrick@nsone.net>
2020-02-23 16:51:31 -0500

    reformat

--- 3rd/geolite2cpp/include/GeoLite2PP.h
+++ 3rd/geolite2cpp/include/GeoLite2PP.h
@@ -4,43 +4,44 @@
 #pragma once
 
 #include <map>
-#include <vector>
-#include <string>
-#include <sstream>
-#include <system_error>
 #include <maxminddb.h>
+#include <sstream>
+#include <string>
+#include <system_error>
+#include <vector>
 
 namespace GeoLite2PP {
 
-class DB final {
- public:
-  ~DB(void);
-  DB(const std::string &database_filename);
+class DB final
+{
+public:
+    DB(const std::string &database_filename);
+    ~DB(void);
 
-  void get_geoinfo(const char *ip_address, uint16_t &country,
-                   uint16_t &prov, uint16_t &isp, uint16_t &city);
+    void get_geoinfo(const char *ip_address, uint16_t &country,
+        uint16_t &prov, uint16_t &isp, uint16_t &city);
 
- private:
-  /* Internal handle to the database. */
-  MMDB_s mmdb;
+private:
+    /* Internal handle to the database. */
+    MMDB_s mmdb;
 
-  std::string str_;
+    std::string str_;
 
-  /* Look up an IP address.  This returns a raw @p MMDB_lookup_result_s structure. */
-  MMDB_lookup_result_s lookup_raw(const char *ip_address);
+    /* Look up an IP address.  This returns a raw @p MMDB_lookup_result_s structure. */
+    MMDB_lookup_result_s lookup_raw(const char *ip_address);
 
-  /* Return a @p std::map of many of the key fields available when looking up an address. */
-  std::string &get_field(const char *ip_address, const char **v);
-  void get_field(const char *ip_address);
+    /* Return a @p std::map of many of the key fields available when looking up an address. */
+    std::string &get_field(const char *ip_address, const char **v);
+    void get_field(const char *ip_address);
 
-  /* Get a specific field, or an empty string if the field does not exist. */
-  std::string &get_field(MMDB_lookup_result_s *lookup, const char **v);
-  void get_field(MMDB_lookup_result_s *lookup);
+    /* Get a specific field, or an empty string if the field does not exist. */
+    std::string &get_field(MMDB_lookup_result_s *lookup, const char **v);
+    void get_field(MMDB_lookup_result_s *lookup);
 
-  uint16_t city_;
-  uint16_t country_;
-  uint16_t isp_;
-  uint16_t prov_;
+    uint16_t city_;
+    uint16_t country_;
+    uint16_t isp_;
+    uint16_t prov_;
 };
 
 }
