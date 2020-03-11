ac33796db7c7543c99b13a15263b38179946a305
Shannon Weyrick <sweyrick@nsone.net>
2020-03-11 17:42:37 -0400

    add json filter, update template

--- tests/integration.sh
+++ tests/integration.sh
@@ -1,11 +1,14 @@
 #/bin/bash
 
+# we filter out some paths due to probabalistic results
+JSONFILTER='delpaths([["5m","dns","cardinality"]])|delpaths([["5m","period"]])|delpaths([["5m","packets","cardinality"]])'
+
 # run pktvisord ($1) and expect json output to match that found in given file ($2), using pktvisord args in ($3)
 PKTVISORD=$1
 JSONTPT=$2
 shift; shift; shift;
 
-result=`$PKTVISORD "$@"`
+result=`$PKTVISORD "$@" | jq $JSONFILTER`
 status=$?
 if [[ $status -eq 0 ]]
 then
@@ -14,7 +17,7 @@ else
   echo "pktvisor failure"
   exit $status
 fi
-want=`cat "$JSONTPT"`
+want=`cat "$JSONTPT" | jq $JSONFILTER`
 status=$?
 if [[ $status -eq 0 ]]
 then
