e5d443571580c86bcd675bf4bd173073d3d3faa7
Shannon Weyrick <sweyrick@ns1.com>
2021-03-21 20:17:34 -0400

    pcap read and parse benchmarks

--- src/handlers/dns/tests/benchmark_dns.cpp
+++ src/handlers/dns/tests/benchmark_dns.cpp
@@ -4,6 +4,15 @@
 
 #include "../dns.h"
 #include <benchmark/benchmark.h>
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wold-style-cast"
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#pragma clang diagnostic ignored "-Wc99-extensions"
+#pragma GCC diagnostic ignored "-Wpedantic"
+#include <IPv4Layer.h>
+#include <PacketUtils.h>
+#include <PcapFileDevice.h>
+#pragma GCC diagnostic pop
 
 using namespace visor::handler::dns;
 
@@ -27,5 +36,46 @@ static void BM_aggregateDomainLong(benchmark::State &state)
 }
 
 BENCHMARK(BM_aggregateDomainLong);
+
+static void BM_pcapReadNoParse(benchmark::State &state)
+{
+
+    for (auto _ : state) {
+        auto reader = pcpp::IFileReaderDevice::getReader("fixtures/dns_udp_tcp_random.pcap");
+
+        if (!reader->open()) {
+            throw std::runtime_error("Cannot open pcap/pcapng file");
+        }
+
+        pcpp::RawPacket rawPacket;
+        while (reader->getNextPacket(rawPacket)) {
+        }
+
+        reader->close();
+        delete reader;
+    }
+}
+BENCHMARK(BM_pcapReadNoParse);
+
+static void BM_pcapReadParse1(benchmark::State &state)
+{
+
+    for (auto _ : state) {
+        auto reader = pcpp::IFileReaderDevice::getReader("fixtures/dns_udp_tcp_random.pcap");
+
+        if (!reader->open()) {
+            throw std::runtime_error("Cannot open pcap/pcapng file");
+        }
+
+        pcpp::RawPacket rawPacket;
+        while (reader->getNextPacket(rawPacket)) {
+            pcpp::Packet packet(&rawPacket, pcpp::OsiModelTransportLayer);
+        }
+
+        reader->close();
+        delete reader;
+    }
+}
+BENCHMARK(BM_pcapReadParse1);
 
 BENCHMARK_MAIN();
