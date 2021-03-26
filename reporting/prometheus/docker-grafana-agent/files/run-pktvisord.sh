13f0d2214085f27e8b07bb230e452fe0db59f79d
Shannon Weyrick <sweyrick@ns1.com>
2021-03-26 15:34:42 -0400

    docker

--- reporting/prometheus/docker-grafana-agent/files/run-pktvisord.sh
+++ reporting/prometheus/docker-grafana-agent/files/run-pktvisord.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-[[ "$PKTVISOR_ARGS" == "" ]] && PKTVISOR_ARGS="eth0"
+[[ "$PKTVISORD_ARGS" == "" ]] && PKTVISORD_ARGS="eth0"
 
-exec pktvisord --prometheus $PKTVISOR_ARGS
+exec pktvisord --prometheus $PKTVISORD_ARGS
