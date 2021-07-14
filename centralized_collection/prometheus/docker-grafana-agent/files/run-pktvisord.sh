7a92e9f047fdc6abd4f6edff451921577e591156
Robert Lahnemann <rlahnemann@ns1.com>
2021-07-14 17:12:22 -0400

    run-pktvisor.sh IFS fix for args with spaces

--- centralized_collection/prometheus/docker-grafana-agent/files/run-pktvisord.sh
+++ centralized_collection/prometheus/docker-grafana-agent/files/run-pktvisord.sh
@@ -2,4 +2,7 @@
 
 [[ "$PKTVISORD_ARGS" == "" ]] && PKTVISORD_ARGS="eth0"
 
+# allow for space after semi ('; ') for better readability and strip leading spaces in args
+PKTVISORD_ARGS="${PKTVISORD_ARGS//; /;}"
+IFS=$';'
 exec pktvisord --prometheus $PKTVISORD_ARGS
