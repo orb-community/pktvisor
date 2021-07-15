ffe3f2c024b7f8f473b03d20dc8c3a3d575cced0
Robert Lahnemann <54502177+rlahnemann@users.noreply.github.com>
2021-07-15 15:28:19 -0400

    run-pktvisor.sh IFS fix for args with spaces (#111)
    * run-pktvisor.sh IFS fix for args with spaces
    * Add check for presence of semi colon for conditional IFS
    * Update README.md for pktvisor-prom-write with semi colon example

--- centralized_collection/prometheus/docker-grafana-agent/files/run-pktvisord.sh
+++ centralized_collection/prometheus/docker-grafana-agent/files/run-pktvisord.sh
@@ -2,4 +2,10 @@
 
 [[ "$PKTVISORD_ARGS" == "" ]] && PKTVISORD_ARGS="eth0"
 
+if [[ "$PKTVISORD_ARGS" == *';'* ]]; then
+    # allow for space after semi ('; ') for better readability and strip leading spaces in args
+    PKTVISORD_ARGS="${PKTVISORD_ARGS//; /;}"
+    IFS=$';'
+fi
+
 exec pktvisord --prometheus $PKTVISORD_ARGS
