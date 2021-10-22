84d5502a63ca04561304297e0590a8ac2e24df05
Shannon Weyrick <sweyrick@ns1.com>
2021-10-22 09:12:01 -0400

    wrap lock in own scope for better safety

--- src/handlers/dns/DnsStreamHandler.cpp
+++ src/handlers/dns/DnsStreamHandler.cpp
@@ -337,12 +337,14 @@ void DnsMetricsBucket::to_json(json &j) const
 {
 
     bool live_rates = !read_only() && !recorded_stream();
-    auto [num_events, num_samples, event_rate, event_lock] = event_data_locked(); // thread safe
 
-    event_rate->to_json(j, live_rates);
-    num_events->to_json(j);
-    num_samples->to_json(j);
-    event_lock.unlock();
+    {
+        auto [num_events, num_samples, event_rate, event_lock] = event_data_locked(); // thread safe
+
+        event_rate->to_json(j, live_rates);
+        num_events->to_json(j);
+        num_samples->to_json(j);
+    }
 
     std::shared_lock r_lock(_mutex);
 
@@ -516,12 +518,14 @@ void DnsMetricsBucket::new_dns_transaction(bool deep, float to90th, float from90
 }
 void DnsMetricsBucket::to_prometheus(std::stringstream &out, Metric::LabelMap add_labels) const
 {
-    auto [num_events, num_samples, event_rate, event_lock] = event_data_locked(); // thread safe
 
-    event_rate->to_prometheus(out, add_labels);
-    num_events->to_prometheus(out, add_labels);
-    num_samples->to_prometheus(out, add_labels);
-    event_lock.unlock();
+    {
+        auto [num_events, num_samples, event_rate, event_lock] = event_data_locked(); // thread safe
+
+        event_rate->to_prometheus(out, add_labels);
+        num_events->to_prometheus(out, add_labels);
+        num_samples->to_prometheus(out, add_labels);
+    }
 
     std::shared_lock r_lock(_mutex);
 
