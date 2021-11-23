f985246db90bf67fd6e874b562b924c2a873b3ed
Shannon Weyrick <sweyrick@ns1.com>
2021-11-23 15:10:06 -0500

    add pktvisor-dnstap binary

--- src/inputs/static_plugins.h
+++ src/inputs/static_plugins.h
@@ -3,6 +3,7 @@ int import_input_plugins()
 {
     CORRADE_PLUGIN_IMPORT(VisorInputMock);
     CORRADE_PLUGIN_IMPORT(VisorInputPcap);
+    CORRADE_PLUGIN_IMPORT(VisorInputDnstap);
     return 0;
 }
 
