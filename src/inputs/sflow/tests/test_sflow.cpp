a2a5cc9830b0e72c6db172a528d8fbf227a00814
Leonardo Parente <lparente@daitan.com>
2022-01-28 13:45:02 -0400

    Improve code coverage sflow

--- src/inputs/sflow/tests/test_sflow.cpp
+++ src/inputs/sflow/tests/test_sflow.cpp
@@ -11,16 +11,36 @@ TEST_CASE("sflow pcap file", "[sflow][file]")
 
     CHECK_NOTHROW(stream.start());
     CHECK_NOTHROW(stream.stop());
+
+    CHECK(stream.schema_key() == "sflow");
+    CHECK(stream.consumer_count() == 0);
+
+    nlohmann::json j;
+    stream.info_json(j);
+    CHECK(j["module"]["config"]["pcap_file"] == "tests/fixtures/ecmp.pcap");
 }
 
 TEST_CASE("sflow udp socket", "[sflow][udp]")
 {
 
+    std::string bind = "127.0.0.1";
+    uint64_t port = 6343;
+
     SflowInputStream stream{"sflow-test"};
-    stream.config_set("bind", "127.0.0.1");
-    stream.config_set("port", static_cast<uint64_t>(6343));
+    stream.config_set("bind", bind);
+    stream.config_set("port", port);
 
     CHECK_NOTHROW(stream.start());
+
+    auto loop = uvw::Loop::getDefault();
+    auto client = loop->resource<uvw::UDPHandle>();
+    client->once<uvw::SendEvent>([](const uvw::SendEvent &, uvw::UDPHandle &handle) {
+        handle.close();
+    });
+    auto dataSend = std::unique_ptr<char[]>(new char[2]{'b', 'c'});
+    client->send(uvw::Addr{bind, port}, dataSend.get(), 2);
+    client->send(bind, port, nullptr, 0);
+
     CHECK_NOTHROW(stream.stop());
 }
 
