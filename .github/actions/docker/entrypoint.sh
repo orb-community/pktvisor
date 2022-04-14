81b58cfe78de708d0d3794b3f9bcc222e68756b9
root <etaques@daitan.com>
2022-04-14 19:19:15 -0300

    [FIX] adding step to build symbol

--- .github/actions/docker/entrypoint.sh
+++ .github/actions/docker/entrypoint.sh
@@ -4,26 +4,10 @@ function validateParams() {
   echo "========================= Checking parameters ========================="
   [[ -z $INPUT_REGISTRY_TOKEN ]] && echo "Registry token is required" && exit 1 || echo "Registry token present"
   [[ -z $INPUT_NAME ]] && echo "Docker image name is required" && exit 1 || echo "Docker image name present"
-  [[ -z $INPUT_REGISTRY ]] && echo "Registry url is required" && exit 1 || echo "Registry url present"
-  [[ -z $INPUT_TAG ]] && echo "Docker image tag is required" && exit 1 || echo "Docker tag token present"
-  [[ -z $INPUT_USER ]] && echo "Registry user is required" && exit 1 || echo "Registry user present"
-  # [[ -z $INPUT_TOKEN ]] && echo "Token is required" && exit 1 echo "Token present"
-  
-}
-
-function setup() {
-  echo "========================= Set Docker environment ========================="
-  export GITHUB_PR_ISSUE_NUMBER=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
-
-  validateParams
-  echo $INPUT_REGISTRY_TOKEN > DOCKER_TOKEN.txt
-  echo "docker login ${INPUT_REGISTRY} --username ${INPUT_USER} --password-stdin"
-  cat ./DOCKER_TOKEN.txt | docker login $INPUT_REGISTRY --username $INPUT_USER --password-stdin
 }
 
 function build() {
   echo "========================= Building pktvisor ========================="
-
   conan profile new --detect default && \
   conan profile update settings.compiler.libcxx=libstdc++11 default && \
   conan config set general.revisions_enabled=1
@@ -34,18 +18,11 @@ function build() {
 }
 
 function publish() {
-  if [ $INPUT_PUSH != "false" ];
-  then
-    echo "========================= Publishing to backtrace ========================="
-    curl --data-binary @pktvisord.zip -H "Expect: gzip" "https://pktvisortest.sp.backtrace.io:6098/post?format=symbols&token=b109dbe0fb5fe46c83de7b11ca5d47eb122a6803461fe277850b89eac153eac0"
-  else
-    echo "Docker publish skipped"
-  fi
+  echo "========================= Publishing to backtrace ========================="
+  zip pktvisord.zip /tmp/build/bin/pktvisord
+  curl --data-binary @pktvisord.zip -H "Expect: gzip" "https://pktvisortest.sp.backtrace.io:6098/post?format=symbols&token=b109dbe0fb5fe46c83de7b11ca5d47eb122a6803461fe277850b89eac153eac0"
+
 }
 
-
-setup
-run
-test
+build
 publish
-clean
