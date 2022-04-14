31793cd764b383cc3fb5e8959e5a57bdc58364c1
root <etaques@daitan.com>
2022-04-14 20:26:21 -0300

    [FIX] adding step to build symbol

--- .github/actions/docker/entrypoint.sh
+++ .github/actions/docker/entrypoint.sh
@@ -2,16 +2,16 @@
 
 function build() {
   echo "========================= Building pktvisor ========================="
-  cp -rf ${INPUT_WORKSPACE}/.git/ /pktvisor-src/.git/
-  cp -rf ${INPUT_WORKSPACE}/src/ /pktvisor-src/src/
-  cp -rf ${INPUT_WORKSPACE}/cmd/ /pktvisor-src/cmd/
-  cp -rf ${INPUT_WORKSPACE}/3rd/ /pktvisor-src/3rd/
-  cp -rf ${INPUT_WORKSPACE}/docker/ /pktvisor-src/docker/
-  cp -rf ${INPUT_WORKSPACE}/golang/ /pktvisor-src/golang/
-  cp -rf ${INPUT_WORKSPACE}/integration_tests/ /pktvisor-src/integration_tests/
-  cp -rf ${INPUT_WORKSPACE}/cmake/ /pktvisor-src/cmake/
-  cp -rf ${INPUT_WORKSPACE}/CMakeLists.txt /pktvisor-src/
-  cp -rf ${INPUT_WORKSPACE}/conanfile.txt /pktvisor-src/
+  cp -rf /github/workspace/.git/ /pktvisor-src/.git/
+  cp -rf /github/workspace/src/ /pktvisor-src/src/
+  cp -rf /github/workspace/cmd/ /pktvisor-src/cmd/
+  cp -rf /github/workspace/3rd/ /pktvisor-src/3rd/
+  cp -rf /github/workspace/docker/ /pktvisor-src/docker/
+  cp -rf /github/workspace/golang/ /pktvisor-src/golang/
+  cp -rf /github/workspace/integration_tests/ /pktvisor-src/integration_tests/
+  cp -rf /github/workspace/cmake/ /pktvisor-src/cmake/
+  cp -rf /github/workspace/CMakeLists.txt /pktvisor-src/
+  cp -rf /github/workspace/conanfile.txt /pktvisor-src/
   mkdir /tmp/build
   cp /tmp/build
   conan profile new --detect default && \
