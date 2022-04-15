c60dbf7e1df21819da36e7f5c3b801996542d045
root <etaques@daitan.com>
2022-04-15 12:36:39 -0300

    [FIX] build pktvisor-cli

--- .github/actions/build-go/entrypoint.sh
+++ .github/actions/build-go/entrypoint.sh
@@ -5,13 +5,13 @@ function build() {
   echo "========================= Building pktvisor-cli ========================="
   cp -rf golang/ /src/
   cp -rf ./version.go /src/pkg/client/version.go
+  go mod tidy
   go build -o pktvisor-cli cmd/pktvisor-cli/main.go
   ls -lha
 }
 
 function copy() {
   echo "========================= Compacting binary and copying ========================="
-  cd /tmp/build
   cp -rf /tmp/build/bin/pktvisor-cli /github/workspace/
 }
 
