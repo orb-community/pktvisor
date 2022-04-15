812f7289475944878dc6b99975aafa5ba8e9ed9c
root <etaques@daitan.com>
2022-04-15 13:04:38 -0300

    [FIX] test uploading all artifacts

--- .github/actions/build-go/entrypoint.sh
+++ .github/actions/build-go/entrypoint.sh
@@ -7,12 +7,11 @@ function build() {
   cp -rf ./version.go /src/pkg/client/version.go
   cd /src
   go build -o pktvisor-cli cmd/pktvisor-cli/main.go
-  ls -lha
 }
 
 function copy() {
-  echo "========================= Compacting binary and copying ========================="
-  cp -rf /tmp/build/bin/pktvisor-cli /github/workspace/
+  echo "========================= Copying binary ========================="
+  cp -rf /src/pktvisor-cli /github/workspace/
 }
 
 build
