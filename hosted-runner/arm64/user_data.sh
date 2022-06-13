66f2f37b9ee6eb4c448119e58c060ac815e92dce
etaques <97463920+etaques@users.noreply.github.com>
2022-06-13 12:31:28 -0300

    [FIX] fix for arm64

--- hosted-runner/arm64/user_data.sh
+++ hosted-runner/arm64/user_data.sh
@@ -34,11 +34,11 @@ pip3 install --no-cache-dir \
 pip3 install --upgrade requests
 
 #get git actions self-runner
-curl -o actions-runner-linux-x64-2.291.1.tar.gz -L https://github.com/actions/runner/releases/download/v2.291.1/actions-runner-linux-x64-2.291.1.tar.gz
+curl -o actions-runner-linux-arm64-2.292.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.292.0/actions-runner-linux-arm64-2.292.0.tar.gz
 
 #extract git actions runner installer
-tar xzf ./actions-runner-linux-x64-2.291.1.tar.gz
+tar xzf ./actions-runner-linux-arm64-2.292.0.tar.gz
 
-RUNNER_ALLOW_RUNASROOT="1" ./config.sh --url https://github.com/ns1labs/pktvisor --token RUNNER_TOKEN --name arm64_runner --work _work --runasservice
+RUNNER_ALLOW_RUNASROOT="1" ./config.sh --url https://github.com/etaques/pktvisor --token RUNNER_TOKEN --name arm64_runner --work _work --runasservice
 
 RUNNER_ALLOW_RUNASROOT="1" ./run.sh
