b14f12f534cbf26a9640913a544d7d951a1ab958
etaques <97463920+etaques@users.noreply.github.com>
2022-06-13 10:01:28 -0300

    [FIX] change instance type

--- hosted-runner/arm64/vars.tf
+++ hosted-runner/arm64/vars.tf
@@ -12,7 +12,7 @@ variable "aws_region" {
 variable "amis" {
   type    = map
   default = {
-    //us-east-1 = "ami-0c4f7023847b90238" //ubuntu 20.04 amd64
+#   us-east-1 = "ami-0c4f7023847b90238" //ubuntu 20.04 amd64
     us-east-1 = "ami-0d70a59d7191a8079" //ubuntu 20.04 arm64
   }
 }
@@ -22,5 +22,6 @@ variable "key_name" {
 }
 
 variable "instance_type" {
-  default = "t3a.medium"
+#  default = "t3a.medium"
+   default = "a1.large"
 }
