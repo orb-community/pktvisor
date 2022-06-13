824de362d2eb3d748aac7151247bb7fe64981990
etaques <97463920+etaques@users.noreply.github.com>
2022-06-13 12:02:06 -0300

    [FIX] comment

--- hosted-runner/arm64/vars.tf
+++ hosted-runner/arm64/vars.tf
@@ -12,7 +12,7 @@ variable "aws_region" {
 variable "amis" {
   type    = map
   default = {
-#   us-east-1 = "ami-0c4f7023847b90238" //ubuntu 20.04 amd64
+//   us-east-1 = "ami-0c4f7023847b90238" //ubuntu 20.04 amd64
     us-east-1 = "ami-025fc2c61f9333edc" //ubuntu 20.04 arm64 on us-west-2
   }
 }
@@ -22,6 +22,6 @@ variable "key_name" {
 }
 
 variable "instance_type" {
-#  default = "t3a.medium"
+//  default = "t3a.medium"
    default = "a1.large"
 }
