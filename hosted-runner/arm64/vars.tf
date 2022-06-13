f6cd116c424bbe72b8cde6bce76366839ec77222
etaques <97463920+etaques@users.noreply.github.com>
2022-06-13 12:12:51 -0300

    [FIX] region index

--- hosted-runner/arm64/vars.tf
+++ hosted-runner/arm64/vars.tf
@@ -13,7 +13,7 @@ variable "amis" {
   type    = map
   default = {
 //   us-east-1 = "ami-0c4f7023847b90238" //ubuntu 20.04 amd64
-    us-east-1 = "ami-025fc2c61f9333edc" //ubuntu 20.04 arm64 on us-west-2
+    us-west-2 = "ami-025fc2c61f9333edc" //ubuntu 20.04 arm64 on us-west-2
   }
 }
 
