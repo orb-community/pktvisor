b5775a0e7ec6fabf8954511c0d51b42b41b02447
etaques <97463920+etaques@users.noreply.github.com>
2022-06-18 09:49:41 -0300

    [FIX] remove python

--- hosted-runner/arm64/user_data.sh
+++ hosted-runner/arm64/user_data.sh
@@ -4,7 +4,7 @@ sudo su
 
 #installing packages
 apt-get update -y
-apt-get install apt-transport-https ca-certificates gnupg2 curl tar software-properties-common build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libsqlite3-dev libreadline-dev libffi-dev libbz2-dev python3 python3-pip python3-dev python xvfb libfontconfig1 libfreetype6 xfonts-scalable fonts-liberation fonts-noto-cjk -y
+apt-get install apt-transport-https qemu qemu-user-static ca-certificates gnupg2 curl tar software-properties-common build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libsqlite3-dev libreadline-dev libffi-dev libbz2-dev python3 python3-pip python3-dev python xvfb libfontconfig1 libfreetype6 xfonts-scalable fonts-liberation fonts-noto-cjk -y
 
 #install docker
 apt-get update -y
@@ -12,26 +12,6 @@ apt-get install docker.io containerd -y
 
 #creating directory git actions runner
 mkdir actions-runner && cd actions-runner
-
-#python stuffs
-pip3 install --no-cache-dir requests unittest-xml-reporting nose mockito pyshould
-
-pip3 install --no-cache-dir \
-        behave==1.2.6 \
-        behavex==1.5.4  \
-        selenium==4.1.3 \
-        docker \
-        PyHamcrest \
-        PyYAML \
-        retry==0.9.2 \
-        deepdiff \
-        jsonschema==4.6.0 \
-        mkdocs \
-        mkdocs-material \
-        capybara-py             \
-        xvfbwrapper
-
-pip3 install --upgrade requests
 
 #get git actions self-runner
 curl -o actions-runner-linux-arm64-2.292.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.292.0/actions-runner-linux-arm64-2.292.0.tar.gz
