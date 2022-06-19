d86682158ac55b51bbaeb34b35fc46b4016600f8
etaques <97463920+etaques@users.noreply.github.com>
2022-06-19 10:08:20 -0300

    Update main.tf

--- hosted-runner/amd64/main.tf
+++ hosted-runner/amd64/main.tf
@@ -1,4 +1,25 @@
-resource "aws_instance" "orb-devops" {
+# resource "aws_instance" "orb-devops" {
+#   ami             = lookup(var.amis, var.aws_region)
+#   instance_type   = var.instance_type
+#   security_groups = [aws_security_group.sg_SelfRunner_amd64.id]
+#   key_name        = var.key_name
+#   user_data       = file("user_data.sh")
+#   associate_public_ip_address = true
+#   subnet_id       = "subnet-086909352c7cc4e61"
+#
+#  ebs_block_device {
+#    device_name = "/dev/sda1"
+#    volume_size = 20
+#  }
+#
+#   tags = {
+#     Name            = "orb-pktvisor-self-runner"
+#     Provider        = "terraform"
+#     Role            = "test"
+#   }
+# }
+
+resource "aws_spot_instance_request" "orb-devops" {
   ami             = lookup(var.amis, var.aws_region)
   instance_type   = var.instance_type
   security_groups = [aws_security_group.sg_SelfRunner_amd64.id]
@@ -7,10 +28,18 @@ resource "aws_instance" "orb-devops" {
   associate_public_ip_address = true
   subnet_id       = "subnet-086909352c7cc4e61"
 
+  spot_price             = "0.16"
+  spot_type              = "one-time"
+  wait_for_fulfillment   = "true"
+
+  ebs_block_device {
+    device_name = "/dev/sda1"
+    volume_size = 20
+  }
+
   tags = {
-    Name            = "orb-pktvisor-self-runner"
+    Name            = "orb-pktvisor-spot-self-runner"
     Provider        = "terraform"
     Role            = "test"
   }
 }
-
