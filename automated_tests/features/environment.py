272aaace25b5a5d9571132494b99d8beebd12c2d
manrodrigues <78241475+manrodrigues@users.noreply.github.com>
2022-07-06 13:29:26 -0300

    adapting scenarios to run in parallel processes (#322)

--- automated_tests/features/environment.py
+++ automated_tests/features/environment.py
@@ -6,21 +6,18 @@ PKTVISOR_CONTAINER_NAME = "pktvisor-test"
 
 
 def before_scenario(context, scenario):
-    cleanup_container(PKTVISOR_CONTAINER_NAME)
+    context.containers_id = dict()
     test_config.send_terminal_commands("modprobe -v dummy numdummies=1", sudo=True)
     test_config.send_terminal_commands("ip link set dummy0 up", sudo=True)
 
 
 def after_scenario(context, scenario):
-    cleanup_container(PKTVISOR_CONTAINER_NAME)
-    test_config.send_terminal_commands("rmmod dummy", sudo=True)
+    cleanup_container(PKTVISOR_CONTAINER_NAME, context.containers_id.keys())
 
 
-def cleanup_container(name_prefix):
+def cleanup_container(name_prefix, containers_id):
     docker_client = docker.from_env()
-    containers = docker_client.containers.list(all=True)
-    for container in containers:
-        test_container = container.name.startswith(name_prefix)
-        if test_container is True:
-            container.stop()
-            container.remove()
+    for container_id in containers_id:
+        container = docker_client.containers.get(container_id)
+        container.stop()
+        container.remove()
