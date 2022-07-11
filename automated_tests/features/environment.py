db7d57b4213135ceccbee7234c1e3473eedb103d
Amanda Rodrigues da Silva <arodrigues@daitan.com>
2022-07-11 09:02:48 -0300

    buckets_queue_metrics

--- automated_tests/features/environment.py
+++ automated_tests/features/environment.py
@@ -1,5 +1,6 @@
 import docker
 from steps import test_config
+import hashlib
 
 
 PKTVISOR_CONTAINER_NAME = "pktvisor-test"
@@ -7,12 +8,16 @@ PKTVISOR_CONTAINER_NAME = "pktvisor-test"
 
 def before_scenario(context, scenario):
     context.containers_id = dict()
-    test_config.send_terminal_commands("modprobe -v dummy numdummies=1", sudo=True)
-    test_config.send_terminal_commands("ip link set dummy0 up", sudo=True)
+    # context.scenario_name = str(scenario.name.replace(" ", ""))
+    context.mock_iface_name = hashlib.shake_256(str(scenario.name.replace(" ", "")).encode()).hexdigest(5)
+    test_config.send_terminal_commands(f"ip link add {context.mock_iface_name} type dummy", sudo=True)
+    test_config.send_terminal_commands(f"ip link set {context.mock_iface_name} up", sudo=True)
 
 
 def after_scenario(context, scenario):
     cleanup_container(PKTVISOR_CONTAINER_NAME, context.containers_id.keys())
+    test_config.send_terminal_commands(f"ip link set {context.mock_iface_name} down", sudo=True)
+    test_config.send_terminal_commands(f"ip link delete {context.mock_iface_name} type dummy", sudo=True)
 
 
 def cleanup_container(name_prefix, containers_id):
