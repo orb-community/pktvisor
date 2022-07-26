53cca8779ce0fea0c6d0b6631f4e604cedcf7525
root <etaques@daitan.com>
2022-07-26 16:58:42 -0300

    [FIX] machine profile

--- .github/hosted-runner/arm64/main.tf
+++ .github/hosted-runner/arm64/main.tf
@@ -1,25 +1,4 @@
-#resource "aws_instance" "orb-devops" {
-#  ami             = lookup(var.amis, var.aws_region)
-#  instance_type   = var.instance_type
-#  security_groups = [aws_security_group.sg_SelfRunner_arm64.id]
-#  key_name        = var.key_name
-#  user_data       = file("user_data.sh")
-#  associate_public_ip_address = true
-#  subnet_id       = "subnet-090c967a67234e472"
-#
-#  ebs_block_device {
-#    device_name = "/dev/sda1"
-#    volume_size = 20
-#  }
-#
-#  tags = {
-#    Name            = "orb-pktvisor-self-runner"
-#    Provider        = "terraform"
-#    Role            = "test"
-#  }
-#}
-
-resource "aws_spot_instance_request" "orb-devops" {
+resource "aws_instance" "orb-devops" {
   ami             = lookup(var.amis, var.aws_region)
   instance_type   = var.instance_type
   security_groups = [aws_security_group.sg_SelfRunner_arm64.id]
@@ -28,18 +7,39 @@ resource "aws_spot_instance_request" "orb-devops" {
   associate_public_ip_address = true
   subnet_id       = "subnet-090c967a67234e472"
 
-  spot_price             = "0.16"
-  spot_type              = "one-time"
-  wait_for_fulfillment   = "true"
-
   ebs_block_device {
     device_name = "/dev/sda1"
     volume_size = 20
   }
 
   tags = {
-    Name            = "orb-pktvisor-spot-self-runner"
+    Name            = "orb-pktvisor-self-runner"
     Provider        = "terraform"
     Role            = "test"
   }
 }
+
+#resource "aws_spot_instance_request" "orb-devops" {
+#  ami             = lookup(var.amis, var.aws_region)
+#  instance_type   = var.instance_type
+#  security_groups = [aws_security_group.sg_SelfRunner_arm64.id]
+#  key_name        = var.key_name
+#  user_data       = file("user_data.sh")
+#  associate_public_ip_address = true
+#  subnet_id       = "subnet-090c967a67234e472"
+#
+#  spot_price             = "0.16"
+#  spot_type              = "one-time"
+#  wait_for_fulfillment   = "true"
+#
+#  ebs_block_device {
+#    device_name = "/dev/sda1"
+#    volume_size = 20
+#  }
+#
+#  tags = {
+#    Name            = "orb-pktvisor-spot-self-runner"
+#    Provider        = "terraform"
+#    Role            = "test"
+#  }
+#}
