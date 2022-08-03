9676efc6f579e21825ed60ac6d0b9dd3bf5c5bfa
root <etaques@daitan.com>
2022-08-03 16:39:53 -0300

    [FIX] change ami

--- .github/hosted-runner/arm64/vars.tf
+++ .github/hosted-runner/arm64/vars.tf
@@ -16,7 +16,7 @@ variable "aws_region" {
 variable "amis" {
   type    = map
   default = {
-    us-east-1 = "ami-0888c389af05d881a" //ubuntu 20.04 arm64 on us-east-1
+    us-east-1 = "ami-070650c005cce4203" //ubuntu 22.04 arm64 on us-east-1
   }
 }
 
