76baa46e68a55a705408d0b01c487ba41b832cf2
Leonardo Parente <leonardo.parente@encora.com>
2022-08-26 16:43:44 -0400

    Implement network interface scan and pick default tap (#373)
    * Implement network interface scan and pick default tap
    
    * fix OSX selection, require 'auto' for auto interface
    
    * Move IFScan logic to PcapInput
    
    Co-authored-by: Shannon Weyrick <sweyrick@ns1.com>

--- cmd/pktvisord/main.cpp
+++ cmd/pktvisord/main.cpp
@@ -41,10 +41,11 @@ static const char USAGE[] =
     Taps and Collection Policies may be created by passing the appropriate YAML configuration file to
     --config, and/or by enabling the admin REST API with --admin-api and using the appropriate endpoints.
 
-    Alternatively, for simple use cases you may specify IFACE, which is either a network interface or an
-    IP address (4 or 6). If this is specified, "default" Tap and Collection Policies will be created with
+    Alternatively, for simple use cases you may specify IFACE, which is either a network interface, an
+    IP address (4 or 6), or "auto". If this is specified, "default" Tap and Collection Policies will be created with
     a "pcap" input stream on the specified interfaced, along with the built in "net", "dns", and "pcap"
-    Stream Handler modules attached. Note that this feature may be deprecated in the future.
+    Stream Handler modules attached. If "auto" is specified, the most used ethernet interface will be chosen.
+    Note that this feature may be deprecated in the future.
 
     For more documentation, see https://pktvisor.dev
 
---
+++ src/inputs/pcap/NetworkInterfaceScan.h
@@ -0,0 +1,69 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
+#elif __APPLE__
+#include <net/if.h>
+#include <ifaddrs.h>
+#elif __linux__
+#include <net/if.h>
+#include <linux/if_link.h>
+#include <ifaddrs.h>
+#endif
+#include <string>
+
+namespace visor::input::pcap {
+
+static inline std::string most_used_interface()
+{
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
+    return std::string();
+#elif __APPLE__
+    struct ifaddrs *ifaddr;
+    if (getifaddrs(&ifaddr) == -1) {
+        return std::string();
+    }
+    std::string interface;
+    uint64_t most_used = 0;
+    for (struct ifaddrs *ifa = ifaddr; ifa != nullptr; ifa = ifa->ifa_next) {
+        if (ifa->ifa_addr == nullptr || (ifa->ifa_flags & IFF_LOOPBACK) || !(ifa->ifa_flags & IFF_UP)) {
+            continue;
+        }
+        if (int family = ifa->ifa_addr->sa_family; family == AF_LINK && ifa->ifa_data != nullptr) {
+            if_data *stats = reinterpret_cast<if_data *>(ifa->ifa_data);
+            auto packets = stats->ifi_ipackets + stats->ifi_opackets;
+            if (packets > most_used) {
+                most_used = packets;
+                interface = std::string(ifa->ifa_name);
+            }
+        }
+    }
+    freeifaddrs(ifaddr);
+    return interface;
+#elif __linux__
+    struct ifaddrs *ifaddr;
+    if (getifaddrs(&ifaddr) == -1) {
+        return std::string();
+    }
+    std::string interface;
+    uint64_t most_used = 0;
+    for (struct ifaddrs *ifa = ifaddr; ifa != nullptr; ifa = ifa->ifa_next) {
+        if (ifa->ifa_addr == nullptr || (ifa->ifa_flags & IFF_LOOPBACK) || !(ifa->ifa_flags & IFF_UP)) {
+            continue;
+        }
+        if (int family = ifa->ifa_addr->sa_family; family == AF_PACKET && ifa->ifa_data != nullptr) {
+            struct rtnl_link_stats *stats = reinterpret_cast<rtnl_link_stats *>(ifa->ifa_data);
+            uint64_t packets = stats->tx_packets + stats->rx_packets;
+            if (packets > most_used) {
+                most_used = packets;
+                interface = std::string(ifa->ifa_name);
+            }
+        }
+    }
+    freeifaddrs(ifaddr);
+    return interface;
+#endif
+}
+
+}
