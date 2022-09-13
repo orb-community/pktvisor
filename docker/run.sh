574d96ae2389702988ac81a0e8733824c4d31a5f
Leonardo Parente <leonardo.parente@encora.com>
2022-09-13 11:26:30 -0400

    Add default dbs to docker image

--- docker/run.sh
+++ docker/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 shift
-pktvisord --cp-token "CP_TOKEN" --cp-url "CP_URL" --cp-path "/usr/local/sbin/crashpad_handler" "$@" &
+pktvisord --cp-token "CP_TOKEN" --cp-url "CP_URL" --cp-path "/usr/local/sbin/crashpad_handler" --default-geo-city "/geo-db/city.mmdb" --default-geo-asn "/geo-db/asn.mmdb" "$@" &
 echo $! > /var/run/pktvisord.pid
