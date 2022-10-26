71ec1314f4b74c573f0a2fdf15292ea2d6bbfd4e
Leonardo Parente <leonardo.parente@encora.com>
2022-10-26 14:20:38 -0400

    PingProbe fixes for MSVC (#508)

--- src/inputs/dnstap/FakeDnstapInputStream.h
+++ src/inputs/dnstap/FakeDnstapInputStream.h
@@ -1,10 +1,12 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
-* License, v. 2.0. If a copy of the MPL was not distributed with this
-* file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 #pragma once
 
 #include "InputStream.h"
+
+namespace visor::input::dnstap {
 
 class DnstapInputStream : public visor::InputStream
 {
