3366fb471c434dd6722fd4acee238a505fed977e
etaques <97463920+etaques@users.noreply.github.com>
2022-11-29 17:58:41 -0300

    Update entrypoint.sh

--- .github/actions/build-cpp/entrypoint.sh
+++ .github/actions/build-cpp/entrypoint.sh
@@ -21,7 +21,7 @@ function build() {
   mkdir /tmp/build
   cd /tmp/build
   conan profile new --detect default && \
-  conan profile update settings.compiler.libcxx=libstdc++11 default && \
+  conan profile update settings.compiler.libcxx=libstdc++11 $INPUT_SETTING default && \
   conan config set general.revisions_enabled=1
   PKG_CONFIG_PATH=/local/lib/pkgconfig cmake -DCMAKE_BUILD_TYPE=$INPUT_BUILD_TYPE -DASAN=$INPUT_ASAN /pktvisor-src && \
   make all -j 4
