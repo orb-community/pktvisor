a4ba29803cf2f10d3651f127b4b2c5476a56c3e1
etaques <97463920+etaques@users.noreply.github.com>
2022-11-29 14:32:58 -0300

    fix sg

--- .github/hosted-runner/arm32/main.tf
+++ .github/hosted-runner/arm32/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "orb-devops" {
   ami             = lookup(var.amis, var.aws_region)
   instance_type   = var.instance_type
-  security_groups = [aws_security_group.sg_SelfRunner_arm64.id]
+  security_groups = [aws_security_group.sg_SelfRunner_arm32.id]
   key_name        = var.key_name
   user_data       = file("user_data.sh")
   associate_public_ip_address = true
