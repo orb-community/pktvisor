c5322a8d288119edb5f96d85694bd5e81ce67a21
etaques <97463920+etaques@users.noreply.github.com>
2022-11-30 08:18:46 -0300

    Update user_data.sh

--- .github/hosted-runner/arm32/user_data.sh
+++ .github/hosted-runner/arm32/user_data.sh
@@ -1,30 +1,42 @@
 #!/bin/sh
 
 #installing packages
-dpkg --add-architecture armhf
+#dpkg --add-architecture armhf
 
 apt-get update -y
 apt-get install apt-transport-https curl -y
 
 # add GPG key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 
 RELEASE=$(lsb_release -c | cut -f2)
 
 # add repos
-add-apt-repository \
-   "deb [arch=armhf] https://download.docker.com/linux/ubuntu \
-   $RELEASE \
-   stable"
+# add-apt-repository \
+#    "deb [arch=armhf] https://download.docker.com/linux/ubuntu \
+#    $RELEASE \
+#    stable"
 
-apt-get update -y
-apt-get install docker-ce:armhf -y
+# apt-get update -y
+# apt-get install docker-ce:armhf -y
 
 apt-get update -y
 apt-get install qemu qemu-user-static ca-certificates gnupg2 curl tar software-properties-common build-essential zlib1g-dev \
 libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libsqlite3-dev libreadline-dev libffi-dev libbz2-dev python3 python3-pip python3-dev python2 xvfb \
-libfontconfig1 libfreetype6 xfonts-scalable fonts-liberation fonts-noto-cjk g++-10-arm-linux-gnueabihf cmake \
-gcc-10-arm-linux-gnueabihf gcc-10-arm-linux-gnueabihf-base python3-venv tcpreplay -y
+libfontconfig1 libfreetype6 xfonts-scalable fonts-liberation fonts-noto-cjk g++-10-arm-linux-gnueabihf cmake openssl \
+gcc-10-arm-linux-gnueabihf gcc-10-arm-linux-gnueabihf-base python3-venv tcpreplay docker-ce containerd -y
+
+wget https://www.openssl.org/source/openssl-1.1.1k.tar.gz
+tar zxf openssl-1.1.1k.tar.gz
+make
+make install
+ldconfig
+
+wget https://zlib.net/zlib-1.2.11.tar.gz
+tar xvf zlib-1.2.11.tar.gz
+./configure
+make all install
+ldconfig
 
 mkdir /etc/systemd/system/docker.service.d
 
