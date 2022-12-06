34e01d6b47c1cd72f9bcff974c4c2818561e44fa
etaques <97463920+etaques@users.noreply.github.com>
2022-12-06 17:23:42 -0300

    fix ami image

--- .github/hosted-runner/arm32/vars.tf
+++ .github/hosted-runner/arm32/vars.tf
@@ -16,7 +16,7 @@ variable "aws_region" {
 variable "amis" {
   type    = map
   default = {
-    us-east-1 = "ami-0c4f7023847b90238" //ubuntu 20.04 amd64 on us-east-1
+    us-east-1 = "ami-070650c005cce4203" //ubuntu 20.04 arm64 on us-east-1
   }
 }
 
