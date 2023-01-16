3eb1e2d712674f03c9a15e3042699323af40d1eb
Leonardo Parente <leonardo.parente@encora.com>
2023-01-16 17:12:13 -0400

    Fix allow default version (#611)

--- src/CoreRegistry.cpp
+++ src/CoreRegistry.cpp
@@ -125,8 +125,7 @@ void CoreRegistry::configure_from_yaml(YAML::Node &node)
 
     if (!node["version"]) {
         _logger->info("missing version, using version \"1.0\"");
-    }
-    if (!node["version"].IsScalar() || node["version"].as<std::string>() != "1.0") {
+    } else if (!node["version"].IsScalar() || node["version"].as<std::string>() != "1.0") {
         throw ConfigException("unsupported version");
     }
 
