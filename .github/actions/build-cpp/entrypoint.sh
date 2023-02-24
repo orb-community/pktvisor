49743bac4c72d566f5b57ee07f65c5f82ce07b3e
Leonardo Parente <leonardo.parente@encora.com>
2023-02-24 15:26:30 -0400

    remove check id

--- .github/actions/build-cpp/entrypoint.sh
+++ .github/actions/build-cpp/entrypoint.sh
@@ -22,6 +22,7 @@ function build() {
   mkdir /tmp/build
   cd /tmp/build
   cp -rf /pktvisor-src/build/conan_home/ .
+  chmod -R 777 /tmp/build/conan_home/
   conan profile new --detect default
   conan profile update settings.compiler.libcxx=libstdc++11 default
   conan config set general.revisions_enabled=1
@@ -31,13 +32,13 @@ function build() {
 
 function move() {
   echo "========================= Compacting binary and copying ========================="
-  id
   cd /tmp/build
   cp -rf /tmp/build/bin/pktvisord /github/workspace/
   strip -s /tmp/build/bin/crashpad_handler
   cp -rf /tmp/build/bin/crashpad_handler /github/workspace/
   cp -rf /tmp/build/bin/pktvisor-reader /github/workspace/
   cp -rf /tmp/build/VERSION /github/workspace/
+  chmod -R 777 /tmp/build/conan_home/
   cp -rf /tmp/build/conan_home/ /github/workspace/build/
   cp -rf /pktvisor-src/golang/pkg/client/version.go /github/workspace/version.go
   cp -rf /pktvisor-src/src/tests/fixtures/pktvisor-port-service-names.csv /github/workspace/custom-iana.csv
