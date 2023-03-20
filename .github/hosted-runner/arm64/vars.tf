f65be01472f5058031ddb071a397d8d49fba09b7
Everton H. Taques <97463920+etaques@users.noreply.github.com>
2023-03-20 16:38:10 -0300

    add image ubuntu 22.04

--- .github/hosted-runner/arm64/vars.tf
+++ .github/hosted-runner/arm64/vars.tf
@@ -16,7 +16,7 @@ variable "aws_region" {
 variable "amis" {
   type    = map
   default = {
-    us-east-1 = "ami-070650c005cce4203" //ubuntu 22.04 arm64 on us-east-1
+    us-east-1 = "ami-0f9bd9098aca2d42b" //ubuntu 22.04 arm64 on us-east-1
   }
 }
 
