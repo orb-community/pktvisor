20403fb3d75ca9cec30d4c001608b6e8b180a31d
Leonardo Parente <23251360+leoparente@users.noreply.github.com>
2023-10-07 16:18:13 -0400

    migrate from catch2v2 to catch2v3 and implement VisorTest helper lib (#689)
    * migrate from catch2v2 to catch2v3 and implement VisorTest helper lib
    
    * Fix win build
    
    * Replach benchmark with catch2 benchmark
    
    * Fix identation
    
    * try to use catch all

--- src/handlers/dns/v1/tests/test_dns_layer.cpp
+++ src/handlers/dns/v1/tests/test_dns_layer.cpp
@@ -1,4 +1,6 @@
-#include <catch2/catch.hpp>
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers.hpp>
+#include <catch2/catch_test_visor.hpp>
 
 #include "DnsStreamHandler.h"
 #include "GeoDB.h"
